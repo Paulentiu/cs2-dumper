053a7dc6b20da532fbf5e9f0a1e01e15ea7b86c0
Paulentiu <jannesxd0@gmail.com>
2026-05-01 10:42:04 +0300

    Update offsets

--- output/engine2_dll.cs
+++ output/engine2_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-04-30 09:11:23.855596600 UTC
+// 2026-05-01 07:40:19.783419100 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: engine2.dll
