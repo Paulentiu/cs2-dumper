c913b884d8f0642b3d08b51a15b4f27d4f000c5c
Paulentiu <jannesxd0@gmail.com>
2026-08-20 09:59:13 +0300

    m

--- output/networksystem_dll.cs
+++ output/networksystem_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-08-13 05:12:11.948533 UTC
+// 2026-08-20 06:59:04.214649600 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: networksystem.dll
--- output/rendersystemdx11_dll.rs
+++ output/rendersystemdx11_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-08-13 05:12:11.948533 UTC
+// 2026-08-20 06:59:04.214649600 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
