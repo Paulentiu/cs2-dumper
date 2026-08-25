fb437faf0a76ecd5657179fd02dde5a17c4afaa2
Paulentiu <jannesxd0@gmail.com>
2026-08-25 12:14:46 +0300

    nice

--- output/networksystem_dll.rs
+++ output/networksystem_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-08-20 06:59:04.214649600 UTC
+// 2026-08-25 09:14:33.034605900 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
--- output/networksystem_dll.zig
+++ output/networksystem_dll.zig
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-08-20 06:59:04.214649600 UTC
+// 2026-08-25 09:14:33.034605900 UTC
 
 pub const cs2_dumper = struct {
     pub const schemas = struct {
